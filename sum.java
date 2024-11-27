effd5623fbef93d643c6576267722fb82513cf61
Sarmista <sarmistarath@gmail.com>
2024-11-27 21:47:22 +0530

    added changes

--- sum.java
+++ sum.java
@@ -1,10 +1,9 @@
-public class sum{
-public static void main(string[] args){
-	System.out.println("hello, this is sum");
-	System.out.println("i like addition");
-	int a=2;
-	int b=3;
-	System.out.println(a+b);
-   }
+public class sum {
+    public static void main(String[] args) {
+        System.out.println("hello, this is sum");
+        System.out.println("i like addition");
+        int a = 2;
+        int b = 3;
+        System.out.println(a + b);
+    }
 }
-	
